4e05ec5104c3293954f5d1046ce71bcea94b7c99
warriorg <warrior.gao@gmail.com>
2014-01-02 19:31:11 +0800

    修稿钱`

---
+++ Classes/View/JgmyView.h
@@ -0,0 +1,13 @@
+//
+//  JgmyView.h
+//  Estates
+//
+//  Created by warrior gao on 13-12-28.
+//  Copyright (c) 2013年 warriorg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JgmyView : UIView
+
+@end
