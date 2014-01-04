a5ed602eb8e5918d28a3afb588e3c5f076ba935b
warriorg <warrior.gao@gmail.com>
2014-01-04 17:59:56 +0800

    最终完成

---
+++ Classes/View/HxtSelectedView.h
@@ -0,0 +1,13 @@
+//
+//  HxtSelectedView.h
+//  Estates
+//
+//  Created by warrior gao on 14-1-4.
+//  Copyright (c) 2014年 warriorg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HxtSelectedView : UIView
+
+@end
---
+++ Classes/View/HxtSelectedView.m
@@ -0,0 +1,31 @@
+//
+//  HxtSelectedView.m
+//  Estates
+//
+//  Created by warrior gao on 14-1-4.
+//  Copyright (c) 2014年 warriorg. All rights reserved.
+//
+
+#import "HxtSelectedView.h"
+
+@implementation HxtSelectedView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
