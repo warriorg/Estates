db3403b275627c9efb1f82ea83d161439fec4754
warriorg <warrior.gao@gmail.com>
2014-01-04 09:25:35 +0800

    修改户型赏析的展示结构

---
+++ Classes/View/BrowseImageView.h
@@ -0,0 +1,13 @@
+//
+//  BrowseImageView.h
+//  Estates
+//
+//  Created by warrior gao on 14-1-3.
+//  Copyright (c) 2014年 warriorg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BrowseImageView : UIView
+
+@end
